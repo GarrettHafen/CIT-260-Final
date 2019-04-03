acc4b79f0f8aa98efc0f9248cb42d0d76b70a96a
nbgillman <nbgillman@DESKTOP-QD5S5SD>
2019-04-02 22:58:45 -0700

    Final Project

---
+++ GillmanHafenFinalProject/Checking.java
@@ -0,0 +1,30 @@
+//File Prologue
+//Name: Nicole Gillman and Garrett Hafen
+//Assignment: Final Project
+//Date: 4/5/19
+//CIT 260
+
+public class Checking {
+	
+	private double balance;
+	
+	/**
+	 * default no-arg contructor
+	 * @return none
+	 */
+	public Checking() {
+		
+	}
+	
+	/**
+	 * constructor for Checking
+	 * @param balance
+	 */
+	public Account(double balance) {
+		
+		
+	}
+	
+	
+
+}
---
+++ GillmanHafenFinalProject/MainFinal.java
@@ -0,0 +1,14 @@
+//File Prologue
+//Name: Nicole Gillman and Garret Hafen
+//Assignment: Final Project
+//Date: 4/5/19
+//CIT 260
+
+public class MainFinal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
